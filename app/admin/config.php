489a12ea6814e9282b197f23b4963d66cbcb68d6
wj008 <26029682@qq.com>
2018-04-28 20:13:42 +0800

    加入工具支持

--- app/admin/config.php
+++ app/admin/config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'sdopx.template_dir' => ['/app/admin/view', '/view/layout'],
+    'sdopx.template_dir' => ['/app/admin/view','/app/admin/zero/view',  '/view/layout'],
     'webname' => 'Beacon快速开发平台',
 ];
