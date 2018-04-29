e4812ec0928b401e86f045296f6bbf60a461ca5d
wj008 <26029682@qq.com>
2018-04-29 23:49:09 +0800

    调整 tool 列表编辑中的排序字段

---
+++ app/tool/libs/MakeInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wj008
+ * Date: 2018/4/28
+ * Time: 16:53
+ */
+
+namespace app\tool\libs;
+
+
+interface MakeInterface
+{
+    public function addUse(string $name);
+}
